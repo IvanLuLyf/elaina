830970776c38bc9b9e66d34fc2bc7acca59b946a
IvanLuLyf <835498692@qq.com>
2020-07-23 22:01:20 +0800

    hide window.Widget

--- eira.js
+++ eira.js
@@ -478,8 +478,8 @@
 
     var initializing = false;
 
-    window.Widget = function () {
-    };
+    function Widget() {
+    }
 
     Widget.extend = function widgetExtends(props) {
         var _super = this.prototype;
